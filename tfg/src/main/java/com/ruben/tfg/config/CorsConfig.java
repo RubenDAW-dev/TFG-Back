29d5ff353cfcb16bbf655194265060820e90cd2b
AzureAD\RubenVillarGonzalez <rvillar@hiberus.com>
2026-03-05 16:49:39 +0100

    no message

--- tfg/src/main/java/com/ruben/tfg/config/CorsConfig.java
+++ tfg/src/main/java/com/ruben/tfg/config/CorsConfig.java
@@ -9,9 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
     }
